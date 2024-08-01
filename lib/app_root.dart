fcc14d1aeeb61fdc5e013f6fe621768b600b71c6
yonggipo <yonggipo@icloud.com>
2024-08-01 12:28:04 +0900

    feat: add folders and files in the mvc format

---
+++ lib/app_root.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+final class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp();
+  }
+}
---
+++ lib/view/signin.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controller/signin_controller.dart';
+
+final class Signin extends GetView<SigninController> {
+  const Signin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
